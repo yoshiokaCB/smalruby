fcfddb944072d539f53b1875971ad7de1ecdb0b0
tm21 <yoshioka@tm-21.com>
2013-12-13 19:15:15 +0900

    modified ball_move.rb

--- samples/ball_move.rb
+++ samples/ball_move.rb
@@ -1,35 +1,55 @@
 require "smalruby"
 Window.width = 690
 
+# ボールの移動スピード
+ball_speed = 5
+
+# ボールの跳ねる力
+ball_power = 20
+
+# 重力の力
+gravity_power = 1
+
+# バウンドの位置
+land = 310
+
+# 壁の位置
+wall = 510
+
 # 境界線
 canvas = Canvas.new
 canvas.on(:start) do
-  box_fill(left: 0, top: 320, right: 690, bottom: 480, color: "yellowgreen")
-  box_fill(left: 540, top: 50, right: 550, bottom: 330, color: "burlywood")
+  box_fill(left: 0, top: land+10, right: 690, bottom: 480, color: "yellowgreen")
+  box_fill(left: wall+30 , top: 50, right: wall+40, bottom: land+20, color: "burlywood")
 end
 
 #ボール設置用のキャンバス準備
-ball = Canvas.new(x: 0, y: 310, width: 30, height: 30)
+ball = Canvas.new(x: 0, y: land, width: 30, height: 30)
 ball.on(:start) do
 
   #キャンバスにボールを表示
   circle_fill(x: 10, y: 10, r: 10, color: "white")
 
-  # y軸移動の初期値　この値を大きくすると大きく跳ねる
-  posi_y = 20
+  # y軸移動の初期値
+  posi_y = ball_power
 
   # ボールの動き
   loop do
 
     #　x軸の移動
-    move 5
-    self.x >= 510 ? turn : turn_if_reach_wall
+    move ball_speed
+    self.x >= wall ? turn : turn_if_reach_wall
 
     #　y軸の移動
     self.y -= posi_y
 
     # ボールの跳ねる動き　（下まで行ったら初期値に戻す）
-    posi_y += self.y >= 310 ? 20 : -2
+    if self.y >= land
+      posi_y = ball_power
+      self.y = land
+    else
+      posi_y -= gravity_power
+    end
 
   end
 
